9e60e382dd1597317e9952f1892b7f63398b974f
Mohamed Sallam <sallamx99@gmail.com>
2020-07-24 15:33:59 +0200

    init backend dev dependencies

---
+++ backend/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+  },
+};
