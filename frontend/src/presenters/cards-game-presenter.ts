73095656be0814e93d33d1526841fbc25201255b
Mohamed Sallam <sallamx99@gmail.com>
2020-07-25 13:20:00 +0200

    finish frontend optional task

--- frontend/src/presenters/cards-game-presenter.ts
+++ frontend/src/presenters/cards-game-presenter.ts
@@ -2,11 +2,6 @@ import { DifficultyLevel, CardsCount } from '@/models/cards-game';
 import { cardsGameRepo } from '@/repositories/cards-game/cards-game-repo';
 
 export function createCardGamePresenter(): CardGamePresenter {
-  let _observers: Function[] = [];
-  let _state: CardsGameState = {
-    type: 'INITIAL_LOADING',
-  };
-  let _solution: number[] = [];
   const _difficultySelections: DifficultySelections = [
     { level: 1, cardsCount: 4 },
     { level: 2, cardsCount: 8 },
@@ -14,24 +9,19 @@ export function createCardGamePresenter(): CardGamePresenter {
   ];
   const _initialDifficulty = 1;
 
+  let _observers: Function[] = [];
+  let _state: CardsGameState = {
+    type: 'INITIAL_LOADING',
+  };
+  let _solution: number[] = [];
+
+
   function _notifySubscribers() {
     _observers.forEach(fn => fn(_state));
   }
-
   function _setState(newState: CardsGameState) {
     _state = newState;
     _notifySubscribers();
-  }
-
-  async function _loadCards(difficultyLevel: DifficultyLevel): Promise<Card[]> {
-    const gameCards = await cardsGameRepo.getNewCards(difficultyLevel);
-    _solution = gameCards.solution;
-    return gameCards.cards.map(value => {
-      return {
-        value,
-        covered: false,
-      };
-    });
   }
   function _decideWinning(cards: Card[], userSolution: number[]): void {
     const currentState = _state as HydratedState;
@@ -52,32 +42,49 @@ export function createCardGamePresenter(): CardGamePresenter {
       });
     }
   }
+  function _initialState(cards: Card[]): CardsGameState {
+    return {
+      type: 'HYDRATED',
+      cards,
+      selectedDifficultyLevel: _initialDifficulty,
+      difficultySelections: _difficultySelections,
+      disableGameSettings: false,
+      hideGameResult: true,
+      userSolution: [],
+      isWin: false,
+    }
+  }
+  async function _loadCards(difficultyLevel: DifficultyLevel): Promise<Card[]> {
+    const gameCards = await cardsGameRepo.getNewCards(difficultyLevel);
+    _solution = gameCards.solution;
+    return gameCards.cards.map(value => {
+      return {
+        value,
+        covered: false,
+      };
+    });
+  }
 
   const presenter: CardGamePresenter = {
     get state(): CardsGameState {
       return _state;
     },
+
     async init(): Promise<void> {
       const cards = await _loadCards(_initialDifficulty);
-      _setState({
-        type: 'HYDRATED',
-        cards,
-        selectedDifficultyLevel: _initialDifficulty,
-        difficultySelections: _difficultySelections,
-        disableGameSettings: false,
-        hideGameResult: true,
-        userSolution: [],
-        isWin: false,
-      });
+      _setState(_initialState(cards));
     },
+
     async selectDifficulty(level: DifficultyLevel): Promise<void> {
       const cards = await _loadCards(level);
       _setState({
         ...(_state as HydratedState),
         disableGameSettings: false,
+        selectedDifficultyLevel: level,
         cards,
       });
     },
+
     play(): void {
       const currentState = _state as HydratedState;
       _setState({
@@ -91,6 +98,7 @@ export function createCardGamePresenter(): CardGamePresenter {
         disableGameSettings: true,
       });
     },
+
     flipCard(index: number): void {
       const currentState = _state as HydratedState;
       const card = currentState.cards[index];
@@ -108,30 +116,26 @@ export function createCardGamePresenter(): CardGamePresenter {
         _decideWinning(cards, userSolution);
       }
     },
+
     async startOver(): Promise<void> {
       const cards = await _loadCards(_initialDifficulty);
-      _setState({
-        ...(_state as HydratedState),
-        cards,
-        userSolution: [],
-        selectedDifficultyLevel: _initialDifficulty,
-        disableGameSettings: false,
-        hideGameResult: true,
-        isWin: false,
-      });
+      _setState(_initialState(cards));
     },
 
     subscribe(observer: (state: CardsGameState) => void): void {
       _observers.push(observer);
     },
+
     destroy(): void {
       _observers = [];
     },
   };
+
   return presenter;
 }
 
 type CardGamePresenter = {
+  state: CardsGameState;
   init(): Promise<void>;
   selectDifficulty(level: DifficultyLevel): Promise<void>;
   play(): void;
@@ -139,7 +143,6 @@ type CardGamePresenter = {
   flipCard(index: number): void;
   subscribe(fn: (state: CardsGameState) => void): void;
   destroy(): void;
-  state: CardsGameState;
 };
 
 type DifficultySelections = Array<{
--- frontend/src/ui/cards-game/HydratedState.vue
+++ frontend/src/ui/cards-game/HydratedState.vue
@@ -1,42 +1,54 @@
 <template>
-  <div class="container">
-    <form class="settings" @submit.prevent="handlePlay">
-      <label for="difficulty-level">Select difficulty level: </label>
-      <select
-        @input="handleDifficultySelection"
-        id="difficulty-level"
-        :disabled="state.disableGameSettings"
-        :value="state.selectedDifficultyLevel"
-        name="difficulty-level"
-        required
-      >
-        <option
-          v-for="selection in state.difficultySelections"
-          :value="selection.level"
-          :key="selection.level"
-          >{{ selection.level }} ({{ selection.cardsCount }} cards)</option
+  <main>
+    <div class="container">
+      <form class="settings" @submit.prevent="handlePlay">
+        <label for="difficulty-level">Select difficulty level: </label>
+        <select
+          @input="handleDifficultySelection"
+          id="difficulty-level"
+          :disabled="state.disableGameSettings"
+          :value="state.selectedDifficultyLevel"
+          name="difficulty-level"
+          required
         >
-      </select>
-      <div>
-        <button type="submit" :disabled="state.disableGameSettings">Play</button>
-      </div>
-    </form>
-    <div class="result" :class="getResultClass(state)">
-      <span>
-        {{ state.isWin ? 'Congrats! You did it' : 'You failed!' }}
-      </span>
-      <button @click="handleStartOver">
-        Start over
-      </button>
-    </div>
-    <div class="card-grid">
-      <template v-for="(card, index) in state.cards">
-        <div :key="card.value" @click="handleFlipCard(index)" :class="getCardClass(card)">
-          {{ card.value }}
+          <option
+            v-for="selection in state.difficultySelections"
+            :value="selection.level"
+            :key="selection.level"
+            >{{ selection.level }} ({{ selection.cardsCount }} cards)</option
+          >
+        </select>
+        <div>
+          <button type="submit" :disabled="state.disableGameSettings">Play</button>
         </div>
-      </template>
+      </form>
+      <div class="result" :class="resultClasses">
+        <span>
+          {{ state.isWin ? 'Congrats! You did it' : 'You failed!' }}
+        </span>
+        <button @click="handleStartOver">
+          Start over
+        </button>
+      </div>
+      <div class="game-container">
+        <div class="card-grid">
+          <template v-for="(card, index) in state.cards">
+            <div :key="card.value" @click="handleFlipCard(index)" :class="getCardClasses(card)">
+              {{ card.value }}
+            </div>
+          </template>
+        </div>
+        <div class="user-solution">
+          Your moves:
+          <ol>
+            <li v-for="move in state.userSolution" :key="move">
+              {{ move }}
+            </li>
+          </ol>
+        </div>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script lang="ts">
@@ -59,6 +71,14 @@ export default Vue.extend({
       required: true,
     } as PropValidator<HydratedState>,
   },
+  computed: {
+    resultClasses(): Record<string, boolean> {
+      return {
+        result: true,
+        hidden: this.state.hideGameResult,
+      };
+    },
+  },
   methods: {
     handleDifficultySelection(event: Event): void {
       const element = event.target as HTMLSelectElement;
@@ -74,16 +94,10 @@ export default Vue.extend({
     handlePlay(): void {
       this.$emit(events.play);
     },
-    getCardClass(card: Card): Record<string, boolean> {
+    getCardClasses(card: Card): Record<string, boolean> {
       return {
         card: true,
         covered: card.covered,
-      };
-    },
-    getResultClass(state: HydratedState): Record<string, boolean> {
-      return {
-        result: true,
-        hidden: state.hideGameResult,
       };
     },
   },
@@ -91,11 +105,16 @@ export default Vue.extend({
 </script>
 
 <style scoped lang="scss">
+main {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
 .container {
   padding: 20px;
   display: flex;
+  width: fit-content;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
   & > * + * {
     margin-top: 20px;
@@ -118,8 +137,7 @@ export default Vue.extend({
     display: flex;
     align-items: center;
     & > * + * {
-      margin-inline-start: 10px;
-      margin-inline-end: 10px;
+      margin-inline-start: 20px;
     }
     select {
       min-width: 60px;
@@ -131,36 +149,75 @@ export default Vue.extend({
     display: flex;
     align-items: center;
     & > * + * {
-      margin-inline-start: 10px;
-      margin-inline-end: 10px;
+      margin-inline-start: 20px;
     }
     &.hidden {
       opacity: 0;
       visibility: hidden;
     }
   }
-  .card-grid {
+  .game-container {
     display: grid;
-    place-items: center;
-    grid-gap: 16px;
-    grid-template-columns: repeat(4, 120px);
-    grid-template-rows: repeat(3, 160px);
-    .card {
+    grid-gap: 20px;
+    @media only screen and (max-width: 768px) {
+      & {
+        grid-template-columns: 1fr;
+      }
+    }
+    @media only screen and (min-width: 768px) {
+      & {
+        grid-template-columns: 1fr auto;
+      }
+    }
+    width: fit-content;
+    .card-grid {
       display: grid;
       place-items: center;
-      font-size: xx-large;
+      grid-gap: 16px;
+      grid-template-columns: repeat(4, 120px);
+      grid-auto-rows:  160px;
+      .card {
+        display: grid;
+        place-items: center;
+        font-size: xx-large;
+        border: 1px solid #9a9a9a;
+        border-radius: 4px;
+        width: 100%;
+        height: 100%;
+        transition: transform 0.6s, color 0.2s step-end;
+        transform-style: preserve-3d;
+      }
+      .covered {
+        cursor: pointer;
+        background-color: #eaeaea;
+        transform: scaleX(-1);
+        color: transparent;
+      }
+    }
+    .user-solution {
       border: 1px solid #9a9a9a;
       border-radius: 4px;
-      width: 100%;
-      height: 100%;
-      transition: transform 0.6s, color 0.2s step-end;
-      transform-style: preserve-3d;
-    }
-    .covered {
-      cursor: pointer;
-      background-color: #eaeaea;
-      transform: scaleX(-1);
-      color: transparent;
+      padding: 8px;
+      min-width: 150px;
+      ol {
+        margin-top: 8px;
+
+        padding: 0;
+        list-style-type: none;
+        counter-increment: customlistcounter;
+        & > * + * {
+          margin-top: 8px;
+        }
+        li {
+          counter-increment: customlistcounter;
+        }
+        li::before {
+          content: counter(customlistcounter) '-  ';
+        }
+        li:first-child {
+          counter-reset: customlistcounter;
+        }
+      }
     }
   }
 }
